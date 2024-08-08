b6f62d5f56a977c331901e18259e2ffdf427b26e
Bruce Wayne <brucethebatmandev@gmail.com>
2024-08-08 09:41:55 +0100

    removed unused imports

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,7 +2,6 @@ import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Postgres from '../assets/postgres.png';
